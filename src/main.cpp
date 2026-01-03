90de02ead7042cbe5538d91594ea78633317419f
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-03 17:02:34 +0200

    add producer-consumer implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include<iostream>
+#include<mutex>
+#include<thread>
+#include<queue>
+#include<condition_variable>
+
+std::mutex glock;																// Mutex for synchronization
+std::condition_variable cond_var;												// Condition Variable for producer-consumer signaling
+std::queue<int> buffer;															// Queue acts as a buffer
+const unsigned int MAX_BUFFER_SIZE = 10;										// Max size of buffer
+
+
+void producer(int value) {
+	std::unique_lock<std::mutex> lock(glock);									// Lock the mutex
+	cond_var.wait(lock,[]() { return buffer.size() < MAX_BUFFER_SIZE; });		// Wait until there is space in the buffer
+	
+	buffer.push(value);
+
+	std::cout << "Producing: " << value << std::endl;
+	std::cout << "Buffer Size after produce: " << buffer.size() << std::endl;
+	
+	lock.unlock();																// Unlock the mutex
+	cond_var.notify_one();														// Notify one waiting thread
+}
+
+void consumer() {
+	std::unique_lock<std::mutex> lock(glock);
+	cond_var.wait(lock, []() { return buffer.size() > 0; });					// Wait until there is something in the buffer
+
+	int value = buffer.front();
+	buffer.pop();
+
+	std::cout << "Consuming: " << value << std::endl;
+	std::cout << "Buffer Size after consume: " << buffer.size() << std::endl;
+
+	lock.unlock();
+	cond_var.notify_one();
+}
 
 int main() {
 
-	std::cout << "Hello world" << std::endl;
+	std::thread producerthread([](){
+		for (int i = 1; i <= 20; i++) {
+			producer(i);					// Create a thread to produce values
+		}
+	});
+	
+	std::thread consumerthread([]() {
+		for (int i = 1; i <= 20; i++) {
+			consumer();					   // Create a thread to consume values
+		}
+	});
+
+	producerthread.join();				   // Wait for producer thread to finish
+	consumerthread.join();				   // Wait for consumer thread to finish
+
+	std::cout << "Main Program Execution..." << std::endl;
 
 	return 0;
 }
