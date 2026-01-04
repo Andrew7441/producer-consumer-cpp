aac096d48a140322617fa59d660d83696ce95496
Andrew <104359161+Andrew7441@users.noreply.github.com>
2026-01-04 20:29:59 +0200

    Add Improvements on .h file

--- include/producer_consumer.h
+++ include/producer_consumer.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <cstddef>
 
-void produce_one(int value);       // push a value into the buffer (thread-safe)
-void consume_one();                 // pop a value, return -1 if empty
-void reset_buffer();               // clear buffer (used by tests)
-bool buffer_empty();               // true if empty
-std::size_t buffer_size();         // current size
+void produce_one(int value);       
+void consume_one();                 
+void reset_buffer();               
+bool buffer_empty();               
+std::size_t buffer_size();         
